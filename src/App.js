b5ba69432561ca2aebbbd16ac7f6ab87fb3ef8d6
Clay Ashby <clay.ashby@gmail.com>
2022-04-30 09:45:33 -0400

    placed spinner on buttons

--- src/App.js
+++ src/App.js
@@ -80,24 +80,29 @@ function App() {
           </Accordion.Body>
         </Accordion.Item>
       </Accordion>
-      <>
-        <Spinner animation="border" variant="primary" />
-        <Spinner animation="border" variant="secondary" />
-        <Spinner animation="border" variant="success" />
-        <Spinner animation="border" variant="danger" />
-        <Spinner animation="border" variant="warning" />
-        <Spinner animation="border" variant="info" />
-        <Spinner animation="border" variant="light" />
-        <Spinner animation="border" variant="dark" />
-        <Spinner animation="grow" variant="primary" />
-        <Spinner animation="grow" variant="secondary" />
-        <Spinner animation="grow" variant="success" />
-        <Spinner animation="grow" variant="danger" />
-        <Spinner animation="grow" variant="warning" />
-        <Spinner animation="grow" variant="info" />
-        <Spinner animation="grow" variant="light" />
-        <Spinner animation="grow" variant="dark" />
-      </>
+      <Spinner animation="border" variant="dark" />
+      <Spinner animation="grow" variant="primary" />
+      <hr />
+      <Button variant="primary" disabled>
+        <Spinner
+          as="span"
+          animation="border"
+          size="sm"
+          role="status"
+          aria-hidden="true"
+        />
+        <span className="visually-hidden">Loading...</span>
+      </Button>{' '}
+      <Button variant="primary" disabled>
+        <Spinner
+          as="span"
+          animation="grow"
+          size="sm"
+          role="status"
+          aria-hidden="true"
+        />
+        Loading...
+      </Button>
     </Container>
   );
 }
