980638a6a062279b3705a6d39a3485199f51e874
Clay Ashby <clay.ashby@gmail.com>
2022-04-30 15:30:46 -0400

    add checkbox click test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,25 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders join our list', () => {
   render(<App />);
-  const linkElement = screen.getByText(/welcome/i);
+  const linkElement = screen.getByText(/join our/i);
   expect(linkElement).toBeInTheDocument();
 });
+test('Submit Order button is disabled', () => {
+  render(<App />);
+  const buttonElement = screen.getByRole('button', { name: /Submit Order/i });
+  expect(buttonElement).not.toBeEnabled();
+  const checkboxElement = screen.getByRole('checkbox', { name: /Accept terms and conditions/i });
+  expect(checkboxElement).not.toBeChecked();
+});
+test('Submit Order button is enabled', () => {
+  render(<App />);
+  const buttonElement = screen.getByRole('button', { name: /Submit Order/i });
+  expect(buttonElement).not.toBeEnabled();
+  const checkboxElement = screen.getByRole('checkbox', { name: /Accept terms and conditions/i });
+  fireEvent.click(checkboxElement);
+  expect(buttonElement).toBeEnabled();
+}
+)
+
