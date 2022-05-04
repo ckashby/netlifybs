1d6b670a2876403de432dacab3860f01e009351e
Clay Ashby <clay.ashby@gmail.com>
2022-05-03 20:46:13 -0400

    Added tests for SummaryForm

--- src/pages/summary/test/SummaryForm.test.jsx
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -5,11 +5,21 @@ test('Checkbox to be unchecked, click enables/disables submit button', () => {
   render(<SummaryForm />);
   const checkbox = screen.getByRole('checkbox');
   expect(checkbox).not.toBeChecked();
-  const submitButton = screen.getByRole('button');
+  const submitButton = screen.getByRole('button', { name: 'Confirm order' });
   expect(submitButton).toBeDisabled();
   fireEvent.click(checkbox);
   expect(submitButton).toBeEnabled();
   fireEvent.click(checkbox);
   expect(submitButton).toBeDisabled();
-
 });
+
+// Test and code checkbox to be unchecked, click enables/disables submit button
+
+
+// Test and code Terms and Conditions bootstrap popover
+
+
+// Test and code Summary Textarea
+
+
+// Test and code Summary Form submit button
