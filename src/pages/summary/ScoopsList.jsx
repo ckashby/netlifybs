cb88a6ba07194f47795be792543d1bab01d08f3f
Clay Ashby <clay.ashby@gmail.com>
2022-05-05 13:15:36 -0400

    Add Scoop menu

--- src/pages/summary/ScoopsList.jsx
+++ src/pages/summary/ScoopsList.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Card, InputGroup, Input, FormControl, Stack } from 'react-bootstrap';
 
 export const ScoopsList = () => {
   return (
@@ -6,7 +7,53 @@ export const ScoopsList = () => {
       <h3>Scoops</h3>
       <p>$2.00 each</p>
       <p>Scoops Total: $6.00</p>
-      <div></div>
+      <Stack direction="horizontal" gap={3}>
+        <div>
+          <Card style={{ width: '12rem', backgroundColor: 'azure' }}>
+            <Card.Img variant="top" src="https://placekitten.com/240/320" />
+            <Card.Body>
+              <InputGroup className="mb-3">
+                <InputGroup.Text>Chocolate</InputGroup.Text>
+                <FormControl
+                  placeholder="0"
+                  aria-label="Chocolate"
+                  aria-describedby="basic-addon2"
+                />
+              </InputGroup>
+            </Card.Body>
+          </Card>
+        </div>
+        <div>
+          <Card style={{ width: '12rem', backgroundColor: 'azure' }}>
+            <Card.Img variant="top" src="https://placekitten.com/240/320" />
+            <Card.Body>
+              <InputGroup className="mb-3">
+                <InputGroup.Text>Vanilla</InputGroup.Text>
+                <FormControl
+                  placeholder="0"
+                  aria-label="Chocolate"
+                  aria-describedby="basic-addon2"
+                />
+              </InputGroup>
+            </Card.Body>
+          </Card>
+        </div>
+        <div>
+          <Card style={{ width: '12rem', backgroundColor: 'azure' }}>
+            <Card.Img variant="top" src="https://placekitten.com/240/320" />
+            <Card.Body>
+              <InputGroup className="mb-3">
+                <InputGroup.Text>Mint Chip</InputGroup.Text>
+                <FormControl
+                  placeholder="0"
+                  aria-label="Chocolate"
+                  aria-describedby="basic-addon2"
+                />
+              </InputGroup>
+            </Card.Body>
+          </Card>
+        </div>
+      </Stack>
     </div>
   );
 };
