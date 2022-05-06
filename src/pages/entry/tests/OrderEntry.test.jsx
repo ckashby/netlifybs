c8b8f31cc742214abb2d77894af32bb669461d14
Clay Ashby <clay.ashby@gmail.com>
2022-05-06 16:11:56 -0400

    added return to arrow function handlers

--- src/pages/entry/tests/OrderEntry.test.jsx
+++ src/pages/entry/tests/OrderEntry.test.jsx
@@ -6,10 +6,10 @@ import OrderEntry from '../OrderEntry';
 test('handles errors for scoops and toppings routes', async () => {
   server.resetHandlers(
     rest.get('http://localhost:3030/scoops', (req, res, ctx) => {
-      res(ctx.status(500));
+      return res(ctx.status(500));
     }),
     rest.get('http://localhost:3030/toppings', (req, res, ctx) => {
-      res(ctx.status(500));
+      return res(ctx.status(500));
     })
   );
 
