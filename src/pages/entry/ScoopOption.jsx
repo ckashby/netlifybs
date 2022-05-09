8a2281522a9a8cf3317af3f36049da768012d07d
Clay Ashby <clay.ashby@gmail.com>
2022-05-09 11:03:25 -0400

    move scoop title below image

--- src/pages/entry/ScoopOption.jsx
+++ src/pages/entry/ScoopOption.jsx
@@ -3,14 +3,14 @@ import { Col, Form } from 'react-bootstrap';
 export default function ScoopOption({ name, imagePath }) {
   return (
     <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: 'center' }}>
-      <h3>{name}</h3>
       <img
         style={{ width: '75%' }}
         src={`http://localhost:3030/${imagePath}`}
         alt={`${name} scoop`}
       />
-      <Form.Label htmlFor='countId'>Number of Scoops</Form.Label>
-      <Form.Control type='number' id='countId' placeholder='0' />
+      <hr />
+      <Form.Label htmlFor='countId'>{name}</Form.Label>
+      <Form.Control type='number' id='countId' placeholder='0' max />
 
     </Col>
   );
--- src/pages/entry/ToppingOption.jsx
+++ src/pages/entry/ToppingOption.jsx
@@ -1,13 +1,16 @@
-import { Col } from 'react-bootstrap';
+import { Col, Form } from 'react-bootstrap';
 
 export default function ToppingOption({ name, imagePath }) {
   return (
     <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: 'center' }}>
+      <h3>{name}</h3>
       <img
-        style={{ width: '75%' }}
+        style={{ width: '50%' }}
         src={`http://localhost:3030/${imagePath}`}
         alt={`${name} topping`}
       />
+      <Form.Check id='toppingId' type="checkbox" label="Add Topping?" />
+      <Form.Control type='number' rows='3' id='countId' placeholder='0' />
     </Col>
   );
 }
