ae9f2f952f00c0e93b3b4a4a2e649e6c2a62b76b
Clay Ashby <clay.ashby@gmail.com>
2022-05-09 11:07:05 -0400

    remove number input from toppingOption

--- src/pages/entry/ToppingOption.jsx
+++ src/pages/entry/ToppingOption.jsx
@@ -3,14 +3,13 @@ import { Col, Form } from 'react-bootstrap';
 export default function ToppingOption({ name, imagePath }) {
   return (
     <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: 'center' }}>
-      <h3>{name}</h3>
       <img
         style={{ width: '50%' }}
         src={`http://localhost:3030/${imagePath}`}
         alt={`${name} topping`}
       />
-      <Form.Check id='toppingId' type="checkbox" label="Add Topping?" />
-      <Form.Control type='number' rows='3' id='countId' placeholder='0' />
+      <hr />
+      <Form.Check id='toppingId' type="checkbox" label={name} />
     </Col>
   );
 }
